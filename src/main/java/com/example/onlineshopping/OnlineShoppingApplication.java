6e88d8d45779d6381188678feacf2647a6ff6bb3
dagi <dagisky@gmail.com>
2021-12-16 17:54:11 -0600

    feat: security added

--- src/main/java/com/example/onlineshopping/OnlineShoppingApplication.java
+++ src/main/java/com/example/onlineshopping/OnlineShoppingApplication.java
@@ -1,15 +1,11 @@
 package com.example.onlineshopping;
 
-import com.example.onlineshopping.domain.Role;
-import com.example.onlineshopping.service.RoleService;
-import com.example.onlineshopping.service.UserService;
+
 import org.modelmapper.ModelMapper;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class OnlineShoppingApplication {
---
+++ src/main/java/com/example/onlineshopping/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.example.onlineshopping.security;
+
+import com.example.onlineshopping.domain.User;
+import com.example.onlineshopping.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+        user.orElseThrow(() -> new UsernameNotFoundException("NOT FOUND"));
+        return new CustomUserDetails(user.get());
+    }
+}
